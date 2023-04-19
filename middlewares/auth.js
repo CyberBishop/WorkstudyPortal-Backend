6bef9053999559447ecc6bdef8aec27414e8f69a
cyberbishop <alabialvin@gmail.com>
2023-04-19 19:59:40 +0100

    update 1 file and delete 1 file

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,30 @@
 require("dotenv").config(); // loading env variables
 const jwt = require("jsonwebtoken");
-const verifyToken = require("../utils/verifyToken");
 
 // MIDDLEWARE FOR AUTHORIZATION (MAKING SURE THEY ARE LOGGED IN)
 const isLoggedIn = async (req, res, next) => {
 	try {
-		const cookie = req.headers.cookie;
-		const token = await verifyToken(cookie);
-		if (token) {
-			req.user = token;
+		// check if auth header exists
+		if (req.headers.authorization) {
+			// parse token from header
+			const token = req.headers.authorization.split(" ")[1]; //split the header and get the token
+			if (token) {
+				const payload = await jwt.verify(token, process.env.JWT_SECRET);
+				if (payload) {
+					// store user data in request object
+					req.user = payload;
+					next();
+				} else {
+					res.status(400).json({ error: "token verification failed" });
+				}
+			} else {
+				res.status(400).json({ error: "malformed auth header" });
+			}
+		} else {
+			res.status(400).json({ error: "No authorization header" });
 		}
-		next();
 	} catch (error) {
-		console.log(error);
-		res.status(400).json({ error: "An error occured" });
+		res.status(400).json({ error });
 	}
 };
 
