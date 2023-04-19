b4747341a8b6efdf6543f2539becefe4baef4072
cyberbishop <alabialvin@gmail.com>
2023-04-19 18:41:16 +0100

    update index.js

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(
-	cors({
-		origin: "*",
-		credentials: true,
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: "*",
+// 		credentials: true,
+// 	})
+// );
+
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "https://workstudy.cu.edu.ng");
+	res.header("Access-Control-Allow-Credentials", true);
+	next();
+});
 
 app.use("/attendances", attendanceRoutes);
 app.use("/users", userRoutes);
