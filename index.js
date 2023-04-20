c1b9b4ff964d03689534345adc93dfaef23d9d77
cyberbishop <alabialvin@gmail.com>
2023-04-20 05:24:41 +0100

    index.js cleanup

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const attendanceRoutes = require("./routes/attendacnceRoutes");
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
-
 require("dotenv").config();
 
 const app = express();
 
+// Parse incoming request body as JSON
 app.use(express.json());
+
+// Set up CORS options
 const corsOptions = {
 	origin: [process.env.CORS_ORIGIN_1, process.env.CORS_ORIGIN_2],
 	credentials: true,
@@ -17,13 +19,14 @@ const corsOptions = {
 	allowedHeaders: "Content-Type,Authorization",
 };
 
+// Enable CORS for all routes
 app.use(cors(corsOptions));
 
+// Set up routes for attendance and user management
 app.use("/attendances", attendanceRoutes);
 app.use("/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
-
+// Connect to MongoDB database
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
@@ -34,14 +37,15 @@ const connectDB = async () => {
 	}
 };
 
-//Routes go here
+// Set up default route to return status message
 app.all("*", (req, res) => {
 	res.json({ status: "server is running" });
 });
 
-//Connect to the database before listening
+// Start server listening after database connection is established
 connectDB().then(() => {
+	const PORT = process.env.PORT || 3000;
 	app.listen(PORT, () => {
-		console.log("listening for requests");
+		console.log(`Server listening on port ${PORT}`);
 	});
 });
